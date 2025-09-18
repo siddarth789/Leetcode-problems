8a7949fa23d78bd96f3471ce5f614b2ecc52832c
siddarth789 <siddarth070511@gmail.com>
2025-09-18 09:41:13 +0530

    Time: 27 ms (61.39%), Space: 19.2 MB (47.78%) - LeetHub

---
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        m=0
+        d={}
+        for i in nums:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]+=1
+        for i in d:
+            if i+1 in d and d[i]+d[i+1]>m:
+                m=d[i]+d[i+1]
+        return m
+
+        
